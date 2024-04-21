e81a5e5ce1f378c97599e01999d6a37e1c912a86
Andy Anthelion <80537973+andy-anthelion@users.noreply.github.com>
2024-04-21 06:04:21 +0000

    following tutorial lol

--- server/server.js
+++ server/server.js
@@ -5,21 +5,25 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+// const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
 });
 
 const PORT = process.env.PORT || 3000;
